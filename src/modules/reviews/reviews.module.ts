5744bf8df55a27bc4a66a822f5b133feef1e3a08
Developer One <developer1@coolmarketing.com>
2025-12-18 14:27:36 -0800

    feat(update): added product reviews to product for edit/delete and create

--- src/modules/reviews/reviews.module.ts
+++ src/modules/reviews/reviews.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Review, ReviewTag, Store, User } from '../../entities';
-import { ReviewsController, ReviewSummariesController } from './reviews.controller';
+import { Product, Review, ReviewTag, Store, User } from '../../entities';
+import { ReviewsController, ProductReviewsController, ReviewSummariesController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review, ReviewTag, Store, User])],
-  controllers: [ReviewsController, ReviewSummariesController],
+  imports: [TypeOrmModule.forFeature([Review, ReviewTag, Store, User, Product])],
+  controllers: [ReviewsController, ProductReviewsController, ReviewSummariesController],
   providers: [ReviewsService],
   exports: [ReviewsService],
 })
